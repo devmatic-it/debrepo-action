fe340a745ecdc416a9e45a7d09363f5778262091
Ferenc Bator <bator@devmatic-it.com>
2019-12-06 21:50:48 +0100

    updated build scipt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -l
 
+set -ex
+
 PACKAGE=$1
 CODENAME="buster"
-DEBFILE_URL="https://github.com/{GITHUB_REPOSITORY}/releases/releases/download/${GITHUB_REF}/${PACKAGE}"
+DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/releases/download/${GITHUB_REF}/${PACKAGE}"
 
 echo "PACKAGE is ${PACKAGE}"
 echo "CODENAME is ${CODENAME}"
 echo "DEBFILE_URL is ${DEBFILE_URL}"
 
+apt-get update -y
+apt-get install -y --no-install-recommends reprepro
 #reprepro --basedir REPOSITORY.PATH includedeb ${CODENAME} ${PACKAGE}
