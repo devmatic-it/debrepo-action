f1fca0937b41ca1c437d80cbcfca43ef0691f908
Ferenc Bator <bator@devmatic-it.com>
2019-12-08 19:47:15 +0100

    Updated wget for dev.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 TAG=$(git tag)
 
-#https://github.com/devmatic-it/debcvescan/releases/download/v0.1.10/debcvescan_0.1.10_linux_amd64.deb
+
 DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/download/${TAG}/${PACKAGE}"
 REPOSITORY="./docs/debian"
 
@@ -13,6 +13,8 @@ echo "CODENAME: ${CODENAME}"
 echo "DEBFILE_URL: ${DEBFILE_URL}"
 echo "REPOSITORY: ${REPOSITORY}"
 OUTPUT=$(ls)
-wget ${DEBFILE_URL}
+
+#wget ${DEBFILE_URL}
+wget https://github.com/devmatic-it/debcvescan/releases/download/v0.1.10/debcvescan_0.1.10_linux_amd64.deb
 echo "Directory list:\n${OUTPUT}"
 #reprepro --basedir ${REPOSITORY} includedeb ${CODENAME} ${PACKAGE}
