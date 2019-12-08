0c0fa704500817ae63d14bb5a471c9effa4b6ef0
Ferenc Bator <bator@devmatic-it.com>
2019-12-08 19:54:19 +0100

    Updated script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,12 @@ echo "PACKAGE: ${PACKAGE}"
 echo "CODENAME: ${CODENAME}"
 echo "DEBFILE_URL: ${DEBFILE_URL}"
 echo "REPOSITORY: ${REPOSITORY}"
-OUTPUT=$(ls)
 
 #wget ${DEBFILE_URL}
 wget https://github.com/devmatic-it/debcvescan/releases/download/v0.1.10/debcvescan_0.1.10_linux_amd64.deb
+
+
+OUTPUT=$(ls)
 echo "Directory list:\n${OUTPUT}"
-#reprepro --basedir ${REPOSITORY} includedeb ${CODENAME} ${PACKAGE}
+echo "Creating Debian Repository"
+reprepro --basedir ${REPOSITORY} includedeb ${CODENAME} ${PACKAGE}
