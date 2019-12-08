470efe916345197a098a1749456f60b8157fe51f
Ferenc Bator <bator@devmatic-it.com>
2019-12-08 12:55:04 +0100

    creating URI

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
-#!/bin/sh -l
+#!/bin/bash
 
 set -ex
 
-#PACKAGE=$1
-#CODENAME="buster"
-#DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/releases/download/${GITHUB_REF}/${PACKAGE}"
+PACKAGE=$1
+CODENAME="buster"
+DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/releases/download/${GITHUB_REF}/${PACKAGE}"
 
-#echo "PACKAGE is ${PACKAGE}"
-#echo "CODENAME is ${CODENAME}"
-#echo "DEBFILE_URL is ${DEBFILE_URL}"
+echo "PACKAGE is ${PACKAGE}"
+echo "CODENAME is ${CODENAME}"
+echo "DEBFILE_URL is ${DEBFILE_URL}"
 
 #apt-get update -y
 #apt-get install -y --no-install-recommends reprepro
