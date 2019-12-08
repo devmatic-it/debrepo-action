4052b325f9637a88243665155900fd1093a0b3e2
Ferenc Bator <bator@devmatic-it.com>
2019-12-08 12:53:31 +0100

    Changed permissions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-PACKAGE=$1
-CODENAME="buster"
-DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/releases/download/${GITHUB_REF}/${PACKAGE}"
+#PACKAGE=$1
+#CODENAME="buster"
+#DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/releases/download/${GITHUB_REF}/${PACKAGE}"
 
-echo "PACKAGE is ${PACKAGE}"
-echo "CODENAME is ${CODENAME}"
-echo "DEBFILE_URL is ${DEBFILE_URL}"
+#echo "PACKAGE is ${PACKAGE}"
+#echo "CODENAME is ${CODENAME}"
+#echo "DEBFILE_URL is ${DEBFILE_URL}"
 
-apt-get update -y
-apt-get install -y --no-install-recommends reprepro
+#apt-get update -y
+#apt-get install -y --no-install-recommends reprepro
 #reprepro --basedir REPOSITORY.PATH includedeb ${CODENAME} ${PACKAGE}
