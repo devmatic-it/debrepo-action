737373aea1387692c2f904aabd03c8dca9020cca
Ferenc Bator <bator@devmatic-it.com>
2019-12-08 19:16:21 +0100

    update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 echo "input Arguments $1 $2 $3"
 
 
-DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/download/${GITHUB_REF}/${PACKAGE}"
+DEBFILE_URL="https://github.com/${GITHUB_REPOSITORY}/releases/download/${PACKAGE}"
 
 echo "GITHUB_REF is ${GITHUB_REF}"
 echo "PACKAGE is ${PACKAGE}"
 echo "CODENAME is ${CODENAME}"
 echo "DEBFILE_URL is ${DEBFILE_URL}"
-echo `ls`
+OUTPUT=$(ls)
+echo "ls: ${OUTPUT}"
 #apt-get update -y
 #apt-get install -y --no-install-recommends reprepro
 #reprepro --basedir REPOSITORY.PATH includedeb ${CODENAME} ${PACKAGE}
